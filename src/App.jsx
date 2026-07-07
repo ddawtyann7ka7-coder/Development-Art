a22fcec070cd39a584616d630b27d0a30c9cd535
ddawtyann7ka7-coder <ddawtyann7ka7@gmail.com>
2026-07-07 16:11:17 +0400

    add optimize performance

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Home from './sections/Home/Home'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
-
-const Syllabus = lazy(() => import('./sections/Syllabus/Syllabus'))
-const WhyUs = lazy(() => import('./sections/WhyUs/WhyUs'))
-const Process = lazy(() => import('./sections/Process/Process'))
-const FAQ = lazy(() => import('./sections/FAQ/FAQ'))
-const RegisterForm = lazy(() => import('./sections/RegisterForm/RegisterForm'))
+import Syllabus from './sections/Syllabus/Syllabus'
+import WhyUs from './sections/WhyUs/WhyUs'
+import Process from './sections/Process/Process'
+import FAQ from './sections/FAQ/FAQ'
+import RegisterForm from './sections/RegisterForm/RegisterForm'
 
 export default function App() {
   const scrollToForm = () => {
@@ -20,13 +18,11 @@ export default function App() {
     <div className="min-h-screen bg-slate-950 text-slate-100 font-sans selection:bg-indigo-500 selection:text-white overflow-x-hidden">
       <Navbar scrollToForm={scrollToForm} />
       <Home scrollToForm={scrollToForm} />
-      <Suspense fallback={<LoadingSpinner />}>
-        <Syllabus />
-        <WhyUs />
-        <Process />
-        <FAQ />
-        <RegisterForm />
-      </Suspense>
+      <Syllabus />
+      <WhyUs />
+      <Process />
+      <FAQ />
+      <RegisterForm />
       <Footer />
       <ScrollToTop />
     </div>
--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react'
-
-export default function LoadingSpinner() {
-    return (
-        <div className="flex justify-center items-center h-[50vh]">
-            <Loader2 className="animate-spin text-indigo-500" size={48} />
-        </div>
-    )
-}
